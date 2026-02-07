37183802c80cc63320fc028b611bbd188bc4ca27
Demarthias <gkube16@gmail.com>
2026-02-06 22:39:06 -0700

    mp4 compress

--- serviceworker.js
+++ serviceworker.js
@@ -1,15 +1,21 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("v1").then(cache => {
+    caches.open("v2").then(cache => {
       return cache.addAll(["/", "/index.html", "/favicon.ico"]);
     })
   );
 });
 
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== "v2").map(key => caches.delete(key)))
+    )
+  );
+});
+
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
